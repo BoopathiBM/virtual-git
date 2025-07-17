12092296c24417aa3672151c597cc0d48878ee4e
boopathi <boopathibm19@gmail.com>
2025-07-17 15:33:12 +0530

    repo layer editied with optional class

--- src/main/java/com/virtualGit/repo/CommitRepository.java
+++ src/main/java/com/virtualGit/repo/CommitRepository.java
@@ -1,5 +1,7 @@
 package com.virtualGit.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.virtualGit.entity.Commit;
 @Repository
 public interface CommitRepository extends JpaRepository<Commit, Long> {
 
+	List<Commit> findByRepositoryId(Long repoId);
 }
--- src/main/java/com/virtualGit/repo/GitRepositoryRepository.java
+++ src/main/java/com/virtualGit/repo/GitRepositoryRepository.java
@@ -1,5 +1,7 @@
 package com.virtualGit.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.virtualGit.entity.GitRepository;
 @Repository
 public interface GitRepositoryRepository extends JpaRepository<GitRepository, Long> {
 
+	List<GitRepository> findByOwnerId(Long ownerId);
 }
