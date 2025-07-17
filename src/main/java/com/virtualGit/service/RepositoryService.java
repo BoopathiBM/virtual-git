2790e3245f780a62dbc1700f206ebf1b76cf00b4
boopathi <boopathibm19@gmail.com>
2025-07-17 16:20:07 +0530

    service layer updated

--- src/main/java/com/virtualGit/service/RepositoryService.java
+++ src/main/java/com/virtualGit/service/RepositoryService.java
@@ -1,7 +1,11 @@
 package com.virtualGit.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 import com.virtualGit.DTO.RepoRequest;
 import com.virtualGit.DTO.RepoResponse;
@@ -12,15 +16,39 @@ import com.virtualGit.repo.UserRepository;
 @Service
 public class RepositoryService {
 
-	@Autowired
-	private GitRepositoryRepository gitRepositoryRepository;
+    @Autowired
+    private GitRepositoryRepository gitRepositoryRepository;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	public RepoResponse createRepository( RepoRequest request){
-		GitRepository repo = new GitRepository();
+    public RepoResponse createRepository(RepoRequest request) {
+        GitRepository repo = new GitRepository();
+        repo.setName(request.name);
+
+        repo.setOwner(userRepository.findById(request.userId)
+                .orElseThrow(() -> new RuntimeException("User not found")));
+        GitRepository saved = gitRepositoryRepository.save(repo);
+
+        RepoResponse response = new RepoResponse();
+        response.id = saved.getId();
+        response.name = saved.getName();
+        response.ownerId = saved.getOwner().getId();
+        return response;
+    }
+
+	public List<RepoResponse> getUserRepositories(Long userId){
 		
+		List<GitRepository> repos = gitRepositoryRepository.findByOwnerId(userId);
+		List<RepoResponse> responses = new ArrayList<>();
+		for(GitRepository r : repos) {
+			RepoResponse res = new RepoResponse();
+			res.id = r.getId();
+			res.name = r.getName();
+			res.ownerId = r.getOwner().getId();
+			responses.add(res);
+		}
+		return responses;
 	}
 
 }
